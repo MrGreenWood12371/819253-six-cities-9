71434ce0a94a54aa976c3b4cdc966a4d91759761
Никита <kondor12371@yandex.ru>
2022-02-16 15:01:48 +0300

    Добавит маршрутизацию и нновую директорию pages
    Добавлена маршрутизация, приватный маршрут и все компонненнты страниц перенесены в директорию pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
-import MainPageScreen from '../main/main-page-screen/main-page-screen';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import FavoritesScreen from '../../pages/favorites/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import MainPageScreen from '../../pages/main/main-page-screen/main-page-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
+import PropertyScreen from '../../pages/property/property-screen/property-screen';
 
 type AppScreenProps = {
   placesCount: number;
@@ -6,7 +14,42 @@ type AppScreenProps = {
 
 function App({placesCount}: AppScreenProps): JSX.Element {
   return (
-    <MainPageScreen placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <MainPageScreen placesCount={placesCount}/>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={
+            <LoginScreen/>
+          }
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={
+            <PropertyScreen/>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <NotFoundScreen/>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,19 @@
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { Navigate, RouteProps } from 'react-router-dom';
+
+type PrivateRouteProps = RouteProps & {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function PrivateRoute(props: PrivateRouteProps) {
+  const {authorizationStatus, children} = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login}/>
+  );
+}
+
+export default PrivateRoute;
